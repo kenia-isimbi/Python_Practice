ff2c6526ec560389001b8ac5a32715a5da5ebd59
kenia-isimbi <kenia@unitzero.ai>
2026-09-18 15:38:06 +0200

    Add ConditionalAction.py to check first day of month

---
+++ ConditionalAction.py
@@ -0,0 +1,9 @@
+# Assign the value 1 to the variable n.
+n = 1
+# Check if the value of n is equal to 1.
+# If the condition is True, print the message indicating the first day of the month.
+if n == 1:
+  print("\nFirst day of a Month!")
+# Print an empty line.
+print()
+
