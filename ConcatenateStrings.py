49c470e93858ce77b6fbc4db142841eb32ab5b75
kenia-isimbi <kenia@unitzero.ai>
2026-09-18 15:36:55 +0200

    Add string concatenation for color list

---
+++ ConcatenateStrings.py
@@ -0,0 +1,5 @@
+list_of_colors = ['Red', 'White', 'Black']  # Create a list of colors containing three elements
+colors = '-'.join(list_of_colors)  # Join the list elements with '-' and store the result in the 'colors' variable
+print()  # Print a blank line for spacing
+print("All Colors: " + colors)  # Print the concatenated colors with '-' between them
+print()  # Print another blank line for spacing
