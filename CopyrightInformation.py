ddc56d61f34d17e48cf78db9a565273d23c955cc
kenia-isimbi <kenia@unitzero.ai>
2026-09-18 15:39:36 +0200

    Add script to display Python copyright information
    This script imports the sys module and displays Python copyright information.

---
+++ CopyrightInformation.py
@@ -0,0 +1,11 @@
+# Import the sys module, which provides access to Python interpreter variables and functions.
+import sys
+
+# Display a header for Python copyright information.
+print("\nPython Copyright Information")
+
+# Display the copyright information about Python using sys.copyright.
+print(sys.copyright)
+
+# Print an empty line for better readability.
+print()
